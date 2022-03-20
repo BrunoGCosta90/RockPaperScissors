c1d5eb2642d869f75f2007be69c71ad3789c0931
BrunoGCosta90 <brunogallegocosta@hotmail.com>
2022-03-20 19:25:29 -0300

    Final version, changes to playRound, playerChoice, computerPlay and added game() function

--- RPSScript.js
+++ RPSScript.js
@@ -1,37 +1,66 @@
+let playerWon = 0;
+let computerWon = 0;
 
 function computerPlay(){
     let randomPick = Math.floor(Math.random()*3);
     switch (randomPick){
       case 0 :
-          randomPick = "Rock";
+          randomPick = "rock";
           break;
       case 1 :
-          randomPick = "Paper";
+          randomPick = "paper";
           break;
       case 2 :
-          randomPick = "Scissors";
+          randomPick = "scissor";
           break;
       default :
-         randomPick = "Paper"; 
+          randomPick = "paper"; 
     }
     return randomPick;
 }
 
 function playerChoice(){
-    let playerPick = prompt("Rock, paper or scissor?", "Rock");
-    return playerPick;
+    let playerPick = prompt("Rock, paper or scissor?", "rock");
+    return playerPick.toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection){
     const player = playerSelection;
     const computer = computerSelection;
+    let result;
 
-    if ((player == "Rock" && computer == "Rock") || (player == "Scissor" && computer == "Scissor") || (player == "Paper" && computer == "Paper")){
-        alert("Draw!");
-    } else if ((player == "Rock" && computer == "Scissor") || (player == "Paper" && computer == "Rock") || (player == "Scissor" && computer == "Paper")){
-        alert("Player won the round!");
+    if ((player == "rock" && computer == "rock") || (player == "scissor" && computer == "scissor") || (player == "paper" && computer == "paper")){
+        //alert("Draw!");
+        return result = "Draw!";
+    } else if ((player == "rock" && computer == "scissor") || (player == "paper" && computer == "rock") || (player == "scissor" && computer == "paper")){
+        //alert("Player won the round!");
+        playerWon++;
+        return result = "Player won the round!";
     } else {
-        alert("Computer won the round!");
+        //alert("Computer won the round!");
+        computerWon++;
+        return result = "Computer won the round!";
     }
 
+}
+
+function game(){
+    let winner;
+    for (let i = 0; i < 5; i++){
+        console.log(playRound(playerChoice(),computerPlay()));
+    }
+
+    if (playerWon > computerWon){
+        playerWon = 0;
+        computerWon = 0;
+        return winner = "Player won the game against the computer!";
+    } else if (playerWon < computerWon){
+        playerWon = 0;
+        computerWon = 0;
+        return winner = "Computer won the game against the player!";
+    } else {
+        playerWon = 0;
+        computerWon = 0;
+        return winner = "It's a draw! No winner!";
+    }
 }
